5543bd9692984dfb2c0fd1b6da61720ef9c92280
md-mehedi-hasan-sohag <sohag7808@gmail.com>
2024-04-28 07:39:18 +0600

    Change Password Done

--- myapp/views.py
+++ myapp/views.py
@@ -1,27 +1,8 @@
-# from django.shortcuts import render,redirect
-# from django.http import HttpResponse
-# from django.contrib.auth import authenticate,login,logout
-# from django.contrib.auth.models import User
-
-# # Create your views here.
-# def home(request):
-#     return render(request,'index.html')
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=username,password=password)
-#         if user is not None:
-#             login(request, user)
-#             return HttpResponse("You are logged in successfully!")
-        
-# def show(request):
-#     return HttpResponse('My name is Mehedi')
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.models import User
 
 def login_view(request):
     if request.method == 'POST':
@@ -40,5 +21,39 @@ def logout_view(request):
     logout(request)
     return render(request, 'index.html')
 
+# @login_required
+# def changePassword(request):
+#     if request.method == 'POST':
+#         current_password = request.POST.get('currentPassword')
+#         new_password = request.POST.get('newPassword')
+#         confirm_password = request.POST.get('confirmPassword')
+
+#         user = request.user
+#         if user.check_password(current_password):
+#             if new_password == confirm_password:
+#                 user.set_password(new_password)
+#                 user.save()
+#                 update_session_auth_hash(request,user)
+#                 return HttpResponse("Password has changed!")
+#     else:
+#         return render(request,'changepassword.html')
+@login_required
 def changePassword(request):
-    return render(request,'changepassword.html')
+    if request.method == 'POST':
+        current_password = request.POST.get('currentPassword')
+        new_password = request.POST.get('newPassword')
+        confirm_password = request.POST.get('confirmPassword')
+
+        user = request.user
+        if user.check_password(current_password):
+            if new_password == confirm_password:
+                user.set_password(new_password)
+                user.save()
+                update_session_auth_hash(request,user)
+                return render(request,'index.html')
+            else:
+                return HttpResponse("New password and confirm password do not match.")
+        else:
+            return HttpResponse("Current password is incorrect.")
+    else:
+        return render(request,'changepassword.html')
